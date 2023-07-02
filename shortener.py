c774bf0c00621fbc80e17ac86515433d206dfa3d
aditya <131573173+heytechz@users.noreply.github.com>
2023-07-02 19:25:05 +0530

    Create shortener.py

---
+++ shortener.py
@@ -0,0 +1,61 @@
+from pyshorteners import Shortener
+def short(link):
+    results = {}
+    
+    # Chilp.it shorten
+    try:
+        s = Shortener()
+        url = s.chilpit.short(link)
+        results["chilp.it"] = url
+    except Exception as error:
+        pass
+    
+    # Click.ru shorten
+    try:
+        s = Shortener()
+        url = s.clckru.short(link)
+        results["click.ru"] = url
+    except:
+        pass
+    
+    # Da.gd shorten
+    try:
+        s = Shortener()
+        url = s.dagd.short(link)
+        results["da.gd"] = url
+    except:
+        pass
+    # Is.gd shorten
+    try:
+        s = Shortener()
+        url = s.isgd.short(link)
+        results["is.gd"] = url
+    except:
+        pass
+    
+    # osdb.link shorten
+    try:
+        s = Shortener()
+        url = s.osdb.short(link)
+        results["osdb.link"] = url
+    except:
+        pass
+    
+    # Qps.ru shorten
+    try:
+        s = Shortener()
+        url = s.qpsru.short(link)
+        results["qps.ru"] = url
+    except:
+        pass
+    
+    # 0x0.st shorten
+    try:
+        s = Shortener(domain='https://0x0.st')
+        url = s.nullpointer.short(link)
+        results["0x0.st"] = url
+    except:
+        pass
+    
+    return results
+      
